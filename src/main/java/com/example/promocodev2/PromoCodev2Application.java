3dbd67a0a57feb313f2ec741984ea2805373d56e
Szymon Wasik <szym.wasik@gmail.com>
2024-05-11 15:42:22 +0200

    project start

---
+++ src/main/java/com/example/promocodev2/PromoCodev2Application.java
@@ -0,0 +1,13 @@
+package com.example.promocodev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCodev2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoCodev2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/promocodev2/PromoCodev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promocodev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoCodev2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
