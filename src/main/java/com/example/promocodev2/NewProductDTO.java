6fcbbf9f57da509638be515599b25b5c60643f55
Szymon Wasik <szym.wasik@gmail.com>
2024-05-11 15:52:20 +0200

    product

---
+++ src/main/java/com/example/promocodev2/NewProductDTO.java
@@ -0,0 +1,7 @@
+package com.example.promocodev2;
+
+import java.math.BigDecimal;
+
+public record NewProductDTO(String name, BigDecimal regular_price, String currency) {
+}
+
---
+++ src/main/java/com/example/promocodev2/Product.java
@@ -0,0 +1,53 @@
+package com.example.promocodev2;
+
+
+import com.fasterxml.jackson.core.io.schubfach.FloatToDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Date;
+@Entity
+public class Product {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Integer productID;
+    private String name;
+    private BigDecimal regular_price;
+    private String currency;
+
+    public Integer getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Integer productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getRegular_price() {
+        return regular_price;
+    }
+
+    public void setRegular_price(BigDecimal regular_price) {
+        this.regular_price = regular_price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
